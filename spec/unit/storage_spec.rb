1f68aaaa69c22b1c93ab0e27abb81d156a4e502b
aetherus <sasabune0122@gmail.com>
2015-11-24 11:42:41 +0800

    Refine tests

--- spec/unit/storage_spec.rb
+++ spec/unit/storage_spec.rb
@@ -4,7 +4,11 @@ describe 'Fife::Operations::Rename' do
   include_examples 'mock'
 
   it 'changes file names' do
-    pipe.pipe(name).pipe(store)
-    Dir[tmpdir.join('*')].should_not be_empty
+    pipe.pipe(-> io {io.write 'Lorem Ipsum'; io}).pipe(name).pipe(store)
+    stored_files = Dir[tmpdir.join('*')]
+    stored_files.count.should == ios.count
+    stored_files.each do |f|
+      File.read(f).should == 'Lorem Ipsum'
+    end
   end
 end
